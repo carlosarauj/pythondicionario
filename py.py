67ed3f6edcda73fd0822417d0efc18c0a44a8d65
Carlos Araújo <111444349+carlosarauj@users.noreply.github.com>
2024-04-23 20:09:40 -0300

    Add files via upload

---
+++ py.py
@@ -0,0 +1,19 @@
+play = {
+    "nome": "Carlos",
+    "idade": 21,
+    "peso": 77,
+    "lvl": 10,
+    'hp': 100,
+    "exp": 0,
+    "dano": 5
+}
+
+npcs = {
+    {"nome": "Monstro", "dano": 50, "hp": 200, "exp": 50},
+    {"nome": "Goblim", "dano": 10 , "hp": 30, "exp": 10},
+    {"nome": "Esqueleto", "dano": 20, "hp": 15, "exp": 20},
+    {"nome": "Zumbi", "dano": 15, "hp": 50, "exp": 20}
+}
+
+print(play)
+#usa o terminal com: python nomedoarquivo.py
